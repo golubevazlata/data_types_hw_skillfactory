2170df996430a3c37dfcb29fd9d476f53d6024ab
Zlata <zlatik_loca@gmail.com>
2023-01-29 22:17:49 +0300

    Add solution

---
+++ script.py
@@ -0,0 +1,10 @@
+
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = int(input("Введите сумму, которую планируете положить под проценты:\n"))
+deposit = []
+for key in per_cent:
+    deposit.append(int(per_cent[key] * money / 100))
+max_deposit = max(deposit)
+print(deposit)
+print("Максимальная сумма, которую вы можете заработать —", max_deposit)
+
